7ccd60f94916d1fb89d2e928cc68bf203b302ed5
JakubJS <elodonskuba@gmail.com>
2017-10-28 11:08:30 +0200

    Start of the project

---
+++ LdifdeGenerator/src/main/java/com/ldifde/LdifdeGenerator/App.java
@@ -0,0 +1,13 @@
+package com.ldifde.LdifdeGenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
